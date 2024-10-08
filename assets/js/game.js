eb30f6a8f1a39aa7b888e516df388731f7066577
“Gökdeniz <gokdenizkeeles@gmail.com>
2024-10-08 15:55:01 +0300

    hafta-seçme

---
+++ assets/js/game.js
@@ -0,0 +1,44 @@
+let hafta = prompt ("1 den 7 ye kadar bir sayı girin ve size olan günü belirleyin")
+
+switch (hafta) {
+ case "1":
+  alert("pazartesi")
+      
+ 
+}
+switch (hafta) {
+  case "2":
+   alert("salı")
+       
+  
+ }
+ switch (hafta) {
+  case "3":
+   alert("çarşamba")
+       
+  
+ }
+ switch (hafta) {
+  case "4":
+   alert("perşembe")
+       
+  
+ }
+ switch (hafta) {
+  case "5":
+   alert("cuma")
+       
+  
+ }
+ switch (hafta) {
+  case "6":
+   alert("cumartesi")
+       
+  
+ }
+ switch (hafta) {
+  case "7":
+   alert("pazar")
+       
+  
+ }
